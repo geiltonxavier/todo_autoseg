b906330734d897d64dce9e5854888a696c4e1dde
Geilton Xavier <geilton@live.com>
2016-07-06 12:06:47 -0400

    add delete for todo item

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -7,8 +7,22 @@ class TodoItemsController < ApplicationController
 		redirect_to @todo_list
 	end
 
+
+	def destroy
+	    @todo_item = @todo_list.todo_items.find(params[:id])
+		respond_to do |format|
+	      if @todo_item.destroy	      	
+	        format.html { redirect_to @todo_list, notice: 'Todo item was successfully destroyed.' }
+	        format.json { render :show, status: :destroyed, location: @todo_list }
+	      else
+	        format.html { render :edit }
+	        format.json { render json: @todo_list.errors, status: :unprocessable_entity }
+	      end
+	    end
+    end
+
 	private
-	
+
 	def set_todo_list
 		@todo_list = TodoList.find(params[:todo_list_id])		
 	end
